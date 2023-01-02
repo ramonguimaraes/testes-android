89dbb1356ae1a11b2e1cfde422ff72e44ca7d18e
ramonapolinario-egsys <ramon.apolinario@egsys.com.br>
2023-01-02 09:39:48 -0300

    mudando a funcao que calcula o salario

--- app/src/main/java/com/ramonguimaraes/testesandroid/CalculatesOvertimeUseCase.kt
+++ app/src/main/java/com/ramonguimaraes/testesandroid/CalculatesOvertimeUseCase.kt
@@ -3,6 +3,6 @@ package com.ramonguimaraes.testesandroid
 class CalculatesOvertimeUseCase {
 
     fun calculate(overtime: Float, wage: Float) : Float {
-        return overtime * wage
+        return overtime * wage * 2
     }
 }
